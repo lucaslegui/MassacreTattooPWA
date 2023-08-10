71b2bf288207d83065c10d437e54521cbcbdbdd8
Lucas Leguizamon <lucaslegui1107@gmail.com>
2023-08-10 00:15:21 -0300

    animaciones con scrollreveal

---
+++ JS/animations.js
@@ -0,0 +1,25 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.navbar', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+sr.reveal('.banner', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-100px'
+});
+
+sr.reveal('main', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-100px'
+});
+
+sr.reveal('footer', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-400px'
+});
